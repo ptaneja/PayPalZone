93f7539d692f37442ecea4e0f00a1cb6f525b387
Tringapps Inc <tringappsinc@Tringappss-MacBook-Pro.local>
2014-05-28 17:11:03 -0400

    Initial Commit

---
+++ PayPalZone/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PayPalZone
+//
+//  Created by Tringapps Inc on 5/28/14.
+//  Copyright (c) 2014 TringApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PayPalZone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PayPalZone
+//
+//  Created by Tringapps Inc on 5/28/14.
+//  Copyright (c) 2014 TringApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
